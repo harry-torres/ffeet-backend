6bbde9a022bf8a3d71dc14fa60d95cbcb2e7d29f
Henrique Alexandre Torres <torresrick.dev@gmail.com>
2020-03-31 15:48:04 -0400

    Remove routes folder

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import requireAll from '../utils/requireAll';
-
-const allRoutes = requireAll('routes/.routes').asMap();
-const router = new Router();
-
-allRoutes.forEach(registerRoute => registerRoute(router));
-
-export default router;
